5d7e9e332c172a57e3796e1f881a7400318a20e4
TrungBiDepTrai <trungboy1122@gmail.com>
2023-12-18 01:19:41 +0700

    trung

--- users/users_home.php
+++ users/users_home.php
@@ -200,7 +200,7 @@
 							</div>
 
 						</div>
-						<p><a herf="#" class="btn">Mua ngay</a></p>
+
 					</div>
 
 					<div class="col-lg-5">
@@ -245,7 +245,7 @@
 							<li>Khám phá những hương vị mới</li>
 							<li>Thuận tiện cho việc tìm nguyên liệu</li>
 						</ul>
-						<p><a herf="#" class="btn">Đi thôi</a></p>
+
 					</div>
 				</div>
 			</div>
